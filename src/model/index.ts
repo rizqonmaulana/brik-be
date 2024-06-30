24684942f33baeb376ed2f382ca98ba7eca92fda
Rizqon Maulana <rizqon@embreo.global>
2024-06-30 17:46:35 +0700

    remove sequelize config from model/index.ts

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,7 +1,3 @@
-import { Sequelize } from "sequelize";
-import * as pg from "pg";
-import dotenv from "dotenv";
-
 import Cart from './cart';
 import Category from './category';
 import Order from './order';
@@ -10,15 +6,7 @@ import Product from './product';
 import ProductOrder from './productOrder';
 import User from './user';
 
-dotenv.config();
-
-const sequelize = new Sequelize(`${process.env.DATABASE_URL}`, {
-    dialectModule: pg,
-    dialect: "postgres",
-  });
-
 export {
-  sequelize,
   Cart,
   Category,
   Order,
